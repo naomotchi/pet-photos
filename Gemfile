e3030c9f57e33e6053028eb3648f96f69d41e9d0
naomotchi <mukorobinanaoki@gmail.com>
2019-09-08 18:11:12 +0900

    post new create

--- Gemfile
+++ Gemfile
@@ -28,7 +28,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'bcrypt', '~> 3.1.7'
 
 # Use ActiveStorage variant
-# gem 'mini_magick', '~> 4.8'
+gem 'mini_magick', '~> 4.8'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
@@ -67,3 +67,5 @@ gem 'bootstrap-sass'
 gem 'haml-rails'
 gem 'erb2haml'
 gem 'devise'
+gem 'font-awesome-rails'
+gem 'font-awesome-sass', '~> 5.4.1'
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,4 +3,43 @@ class PhotosController < ApplicationController
   def index
   end
 
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.create(photo_params)
+    if @photo.save
+      redirect_to post_photos_path
+    else
+      redirect_to :new
+    end
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+  def edit
+    @photo = Photo.find(params[:id]) 
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+    @photo.update(photo_params)
+    redirect_to @photo
+  end
+
+  def destroy
+  end
+
+  def post
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:name, :text, :image)
+  end
+
 end
