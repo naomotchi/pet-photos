2a2e6fd63c4c177fcd45e882b16d84b4da499f18
naomotchi <mukorobinanaoki@gmail.com>
2019-09-20 12:27:09 +0900

    validates&render&view

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,13 @@
 class Photo < ApplicationRecord
   mount_uploader :image, ImageUploader, presence: true
 
+  with_options presence: true do
+  validates :text  
+  validates :name
+  validates :image
+  end
+  
   has_many :comments
-  belongs_to :user
+  belongs_to :user, optional: true
+  has_many :comments,foreign_key: :photo_id, dependent: :destroy
 end
